a3702b419ae1e41929013d1bd0d8a833950f8001
Leandro Galastro <lcgalastro@gmail.com>
2022-04-22 22:11:58 -0300

    controle

---
+++ controle/if2.js
@@ -0,0 +1,10 @@
+function teste1(num) {
+    if(num>7)
+        console.log(num)
+    console.log('Final')
+}
+
+teste1(6)
+teste1(8)
+
+// sem as chaves especificando o bloco da condicional, a condição só aceita uma linha. Indentação em JS só para organização.
